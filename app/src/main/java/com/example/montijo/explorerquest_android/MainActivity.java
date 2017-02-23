ef8b012ff5277552ef099a1b3d5bcd50e5b588fa
BMontijo <juansonos@gmail.com>
2017-02-23 18:57:59 -0500

    Start button launches character selection activity

--- app/src/main/java/com/example/montijo/explorerquest_android/MainActivity.java
+++ app/src/main/java/com/example/montijo/explorerquest_android/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.montijo.explorerquest_android;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // find start button
+        Button start = (Button) findViewById(R.id.button_start);
+
+        // set onClick listener to button
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // create intent to start Character Selection Activity
+                Intent characterSelectionIntent = new Intent(MainActivity.this, CharacterSelectionActivity.class);
+
+                // start the new activity
+                startActivity(characterSelectionIntent);
+            }
+        } );
     }
 }
