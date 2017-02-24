6620296ce932c9298a97c91feea3605d8185e84d
BMontijo <juansonos@gmail.com>
2017-02-24 18:08:15 -0500

    Structure story activity

--- app/src/main/java/com/example/montijo/explorerquest_android/StoryActivity.java
+++ app/src/main/java/com/example/montijo/explorerquest_android/StoryActivity.java
@@ -1,7 +1,8 @@
 package com.example.montijo.explorerquest_android;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
 
 public class StoryActivity extends AppCompatActivity {
 
@@ -9,5 +10,10 @@ public class StoryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_story);
+
+        // find next button
+        Button next = (Button) findViewById(R.id.button_next);
+
+
     }
 }
