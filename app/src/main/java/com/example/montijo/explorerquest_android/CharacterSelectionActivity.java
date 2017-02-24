6153116c50139b108e45bb02c841cac336622578
BMontijo <juansonos@gmail.com>
2017-02-24 17:15:24 -0500

    Create Story Activity and make character buttons launch it

--- app/src/main/java/com/example/montijo/explorerquest_android/CharacterSelectionActivity.java
+++ app/src/main/java/com/example/montijo/explorerquest_android/CharacterSelectionActivity.java
@@ -1,7 +1,10 @@
 package com.example.montijo.explorerquest_android;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class CharacterSelectionActivity extends AppCompatActivity {
 
@@ -9,5 +12,39 @@ public class CharacterSelectionActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_character_selection);
+
+        // find Dora button
+        Button dora = (Button) findViewById(R.id.button_dora);
+
+        // set onClickListener to Dora Button
+        dora.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // TODO add character creation code here
+
+                // create intent to start Story Activity
+                Intent storyIntent = new Intent(CharacterSelectionActivity.this, StoryActivity.class);
+
+                // start the new activity
+                startActivity(storyIntent);
+            }
+        });
+
+        // find Boots button
+        Button boots = (Button) findViewById(R.id.button_boots);
+
+        // set onClickListener to Boots Button
+        boots.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // TODO add character creation code here
+
+                // create intent to start Story Activity
+                Intent storyIntent = new Intent(CharacterSelectionActivity.this, StoryActivity.class);
+
+                // start the new activity
+                startActivity(storyIntent);
+            }
+        });
     }
 }
