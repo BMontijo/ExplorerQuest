f20d374290fbeb16c164d42ff25d7742b27617f2
BMontijo <juansonos@gmail.com>
2017-02-26 16:56:31 -0500

    Create combat activity

---
+++ app/src/main/java/com/example/montijo/explorerquest_android/CombatActivity.java
@@ -0,0 +1,13 @@
+package com.example.montijo.explorerquest_android;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CombatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_combat);
+    }
+}
