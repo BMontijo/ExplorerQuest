fc5bba1335fa1bd7479339b9ab99683a8ec4eb68
BMontijo <juansonos@gmail.com>
2019-05-07 20:24:54 -0400

    Add mediaPlayer TODOs to MainActivity

--- app/src/main/java/com/example/montijo/explorerquest_android/MainActivity.java
+++ app/src/main/java/com/example/montijo/explorerquest_android/MainActivity.java
@@ -7,14 +7,20 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    // Media Player
+    //MediaPlayer mMp;
+
+    //TODO mediaManager listener onComplete loop
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //TODO mediaManager start main music
+
         // find start button
-        Button start = (Button) findViewById(R.id.button_start);
+        Button start = findViewById(R.id.button_start);
 
         // set onClick listener to button
         start.setOnClickListener(new View.OnClickListener() {
@@ -23,9 +29,20 @@ public class MainActivity extends AppCompatActivity {
                 // create intent to start Character Selection Activity
                 Intent characterSelectionIntent = new Intent(MainActivity.this, CharacterSelectionActivity.class);
 
+                //TODO stop mediaManager
+
                 // start the new activity
                 startActivity(characterSelectionIntent);
             }
         } );
+
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        //TODO stop mediaManager
+
     }
 }
